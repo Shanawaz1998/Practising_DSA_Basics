a971b489197f054129449a1087e2425fcca0af2e
Shanawaz1998 <bahadurshanawaz@gmail.com>
2023-07-16 15:24:33 +0530

    Switch Stmt program updated

--- Baisc_Programming_Javed_Sir/src/DSA_KunalKushwaha/SwitchStmtupdates.java
+++ Baisc_Programming_Javed_Sir/src/DSA_KunalKushwaha/SwitchStmtupdates.java
@@ -7,23 +7,41 @@ public class SwitchStmtupdates {
 		Scanner sc = new Scanner(System.in);
 		
 		int no = sc.nextInt();
-		switch(no) {
-		case 1:{
-			System.out.println("Monday");
-			break;
-		}
-		case 2: {
-			System.out.println("Tuesday");
-			break;
-		}
-		case 3: {
-			System.out.println("Wednesday");
-			break;
-		}
-		default:{
-			System.out.println("Please enter the valid no.");
-			break;
-		}
+//		switch(no) {
+//		case 1:{
+//			System.out.println("Monday");
+//			break;
+//		}
+//		case 2: {
+//			System.out.println("Tuesday");
+//			break;
+//		}
+//		case 3: {
+//			System.out.println("Wednesday");
+//			break;
+//		}
+//		default:{
+//			System.out.println("Please enter the valid no.");
+//			break;
+//		}
+//		}
+
+//		switch(no){
+//			case 1 -> System.out.println("Monday");
+//			case 2 -> System.out.println("Tuesday");
+//			case 3 -> System.out.println("Wednesday");
+//			case 4 -> System.out.println("Thursday");
+//			case 5 -> System.out.println("Friday");
+//			case 6 -> System.out.println("Saturday");
+//			case 7 -> System.out.println("Sunday");
+//			default -> System.out.println("Please enter the valid no.");
+//		}
+
+		//To Identify the WeekDays and WeekEnds
+		switch(no){
+			case 1, 2, 3, 4, 5 -> System.out.println("WeekDays");
+			case 6,7 -> System.out.println("WeekEnds");
+			default -> System.out.println("please enter the valid no.");
 		}
 	}
 }
